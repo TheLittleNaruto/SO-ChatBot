2a3b15110a4e59c041a6a1315d9705c70951c2ad
TheLittleNaruto <kumar.gaurav.ciit@gmail.com>
2016-09-05 14:13:24 +0530

    Updated welcome message

--- master.js
+++ master.js
@@ -2089,12 +2089,12 @@ module.exports = function (bot) {
 
 },{}],10:[function(require,module,exports){
 module.exports = function (bot) {
-    var welcomeFmt = 'Welcome to the JavaScript chat! Please review the {0}. ' +
+    var welcomeFmt = 'Welcome to the Java and Android Era chat! Please review the {0}. ' +
         'Please don\'t ask if you can ask or if anyone\'s around; just ask ' +
         'your question, and if anyone\'s free and interested they\'ll help.';
     var rulesLink = bot.adapter.link(
-        'room rules',
-        'http://rlemon.github.com/so-chat-javascript-rules/'
+        'Read common chat FAQs',
+        'http://chat.stackoverflow.com/faq/'
     );
 
     var config = Object.merge(
